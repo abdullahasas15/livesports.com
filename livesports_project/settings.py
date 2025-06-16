054bec27af19ae43b0b1a6ce5d0a8351a8d28673
Mohammad Abdullah <abdullahasas15@gmail.com>
2025-06-16 16:39:01 +0530

    Connected Django to Supabase PostgreSQL

--- livesports_project/settings.py
+++ livesports_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'livesports_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '9440802460live',
+        'HOST': 'db.yhkrlmfgvjjikbwbsmfg.supabase.co',
+        'PORT': '5432',
     }
 }
 
