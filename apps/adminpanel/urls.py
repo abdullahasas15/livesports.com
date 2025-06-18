44fa5e0a4efd7ede5655cb8e6bbd352cb3fb2399
Mohammad Abdullah <abdullahasas15@gmail.com>
2025-06-19 00:11:13 +0530

    admin login done

--- apps/adminpanel/urls.py
+++ apps/adminpanel/urls.py
@@ -1,6 +1,12 @@
+# adminpanel/urls.py
 from django.urls import path
 from . import views
 
+app_name = 'adminpanel'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login/', views.admin_login_signup, name='login_signup'),
+    path('logout/', views.admin_logout_view, name='logout'), # Add logout URL
+    path('create-tournament/', views.create_tournament_view, name='create_tournament'),
+    path('dashboard/', views.admin_dashboard_view, name='dashboard'),
 ]
